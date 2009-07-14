0e3cf67d9b2164cdb12a8f74360a15bec5e851b2
Harald Hvaal <harald.hvaal@gmail.com>
2009-07-14 10:52:53 +0900

    adding event printing to debug interface

--- src/NewUI/debuginterface.cpp
+++ src/NewUI/debuginterface.cpp
@@ -1,17 +1,19 @@
 #include "debuginterface.h"
+#include <QTextEdit>
 #include <QTreeWidget>
 #include <QTreeWidgetItem>
 #include <QVBoxLayout>
+#include <QScrollBar>
 #include "../Misc/Master.h"
 
 class Tree : public QTreeWidget
 {
     public:
-        Master *master;
+        NodeUser *user;
         
-        Tree(QWidget *parent, Master *master)
+        Tree(QWidget *parent, NodeUser *user)
             : QTreeWidget(parent),
-            master(master)
+            user(user)
         {
             setColumnCount(1);
 
@@ -22,6 +24,7 @@ class Tree : public QTreeWidget
                 QTreeWidgetItem *item = new QTreeWidgetItem(this);
                 item->setText(0, QString::fromStdString((*i)->getId()));
                 addChildren(*i, item);
+                (*i)->addRedirection(user);
             }
 
         }
@@ -36,6 +39,7 @@ class Tree : public QTreeWidget
                 QTreeWidgetItem *item = new QTreeWidgetItem(parentItem);
                 item->setText(0, QString::fromStdString((*i)->getId()));
                 addChildren(*i, item);
+                (*i)->addRedirection(user);
             }
 
         }
@@ -51,9 +55,39 @@ DebugInterface::DebugInterface(QWidget *parent, Master *master)
     QVBoxLayout *layout = new QVBoxLayout(this);
     setLayout(layout);
 
-    QTreeWidget *tree = new Tree(this, master);
+    tree = new Tree(this, this);
 
     layout->addWidget(tree);
+
+    text = new QTextEdit(this);
+
+    layout->addWidget(text);
+
+    connect(this, SIGNAL(newEvent(QString)),
+            this, SLOT(receiveEvent(QString)));
+
+}
+
+void DebugInterface::handleEvent(Event &event)
+{
+    //this is the place to make it print actual useful info about the event
+    QString info;
+
+    if (event.type() == Event::ChangeEvent) {
+        info += "(ChangeEvent) ";
+        info += "val:" + QString::number(static_cast<ChangeEvent&>(event).getVal());
+    } else if (event.type() == Event::NewValueEvent) {
+        info += "(NewValueEvent) ";
+    } else {
+        info += "(Unknown event type: " + int(event.type()) + QString(")");
+    }
+    emit newEvent(info);
+}
+
+void DebugInterface::receiveEvent(QString info)
+{
+    text->append(info);
+    text->verticalScrollBar()->setValue(text->verticalScrollBar()->maximum());
 }
 
 
--- src/NewUI/debuginterface.h
+++ src/NewUI/debuginterface.h
@@ -2,16 +2,26 @@
 #define DEBUGINTERFACE_H
 
 #include <QDialog>
+#include "../Controls/Node.h"
 
-class DebugInterface : public QDialog
+class DebugInterface : public QDialog, public NodeUser
 {
     Q_OBJECT
 
     public:
         DebugInterface(QWidget *parent = NULL, class Master *master = NULL);
+        void handleEvent(Event &event);
+
+    signals:
+        void newEvent(QString info);
+
+    private slots:
+        void receiveEvent(QString info);
 
     private:
         class Master *master;
+        class QTextEdit *text;
+        class Tree *tree;
 
 };
 
