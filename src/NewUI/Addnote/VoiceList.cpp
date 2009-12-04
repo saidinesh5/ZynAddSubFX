e33cd94866f48653285963eceafe84850c79c47a
Harald Hvaal <harald.hvaal@gmail.com>
2009-12-04 22:45:57 +0100

    lots of ui improvements

--- src/NewUI/Addnote/VoiceList.cpp
+++ src/NewUI/Addnote/VoiceList.cpp
@@ -10,23 +10,10 @@ VoiceList::VoiceList(QString absoluteId, QWidget *parent)
 
     setupUi(this);
 
-    QWidget *mainWidget = new QWidget;
-    scrollArea->setWidget(mainWidget);
-
-    QLayout *lay = new QVBoxLayout(mainWidget);
-
     Node *node   = Node::get(absoluteId.toStdString());
-    if(node)
-        for(int i = 0; i < node->getChildren().size(); ++i) {
-            VoiceWidget      *w = new VoiceWidget();
-            CollapsableFrame *f = new CollapsableFrame(this, w);
-            f->title->setText("<h3>"
-                              + QString::fromStdString(node->getChildren()[i]->
-                                                       getId()) + "</h3>");
-            lay->addWidget(f);
-            f->setProperty("controlId",
-                           QString::fromStdString(node->getChildren()[i]->getId()));
-        }
+
+    partBar->setControlsWidget(voiceWidget);
+    partBar->setNode(node);
 }
 
 // vim: sw=4 sts=4 et tw=100
