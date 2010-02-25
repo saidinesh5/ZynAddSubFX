2f9170f64bea39d842b9c27e7194ca073403620a
Harald Hvaal <harald.hvaal@gmail.com>
2010-02-25 21:05:04 +0100

    now the vkeybd is a lot more proper

--- src/NewUI/Keyboard.cpp
+++ src/NewUI/Keyboard.cpp
@@ -5,10 +5,24 @@
 #include "../Misc/Master.h"
 #include <QtDebug>
 
-static const int octaves = 3;
+static const int octaves = 5;
 static const int channel = 0;
-static const int rootnote = 12*3;
-static const int tangents = octaves * 12;
+static const int rootnote = 60 - 12; //C-3
+
+//how wide are the black ceys relative to the white ones?
+static const float blackKeyMult = 0.7;
+
+//how high are black keys relative to widget height
+static const float blackKeyHeight = 0.5;
+
+//these two arrays define the correlation between black/white tangent 0-7, and their corresponding
+//midi note (relative to the start of the octave)
+static const int whiteIndex[] = {
+    0, 2, 4, 5, 7, 9, 11
+};
+static const int blackIndex[] = {
+    1, 3, -1, 6, 8, 10, -1
+};
 
 Keyboard::Keyboard(QWidget *parent)
     :QWidget(parent),
@@ -19,9 +33,70 @@ Keyboard::Keyboard(QWidget *parent)
 
 int Keyboard::toNote(const QPoint& pos)
 {
-    int tangentWidth = width() / tangents;
+    const float whiteKeyWidth = float(width()) / (7*octaves);
+    const float blackKeyWidth = whiteKeyWidth * blackKeyMult;
+    const float octaveWidth = float(width()) / octaves;
 
-    return rootnote + pos.x() / tangentWidth;
+    int octave = pos.x() / octaveWidth;
+    int rest = pos.x() - octave * octaveWidth;
+
+    if (pos.x() < 0) return 0;
+    if (pos.x() >= width()) return rootnote + octaves * 12;
+
+    if (pos.y() < blackKeyHeight * height()) {
+        //although there are both white and black keys on the top, we choose to say this is a black key
+        int keyOffset = blackIndex[int((rest - blackKeyWidth / 2) / whiteKeyWidth)];
+
+        //if this is between some black keys, we return the corresponding white key
+        if (-1 == keyOffset) {
+            keyOffset = blackIndex[int((rest - blackKeyWidth / 2) / whiteKeyWidth) - 1];
+        }
+
+        int ret = rootnote + octave * 12 + keyOffset;
+        return ret;
+
+    } else {
+        //we know it's a white key
+        int ret = rootnote + octave * 12 + whiteIndex[int((rest / whiteKeyWidth))];
+        return ret;
+    }
+}
+
+void Keyboard::paintEvent(QPaintEvent *event)
+{
+    QPainter p(this);
+    p.setRenderHint(QPainter::Antialiasing);
+
+    const float whiteKeyWidth = float(width()) / (7*octaves);
+    const float blackKeyWidth = whiteKeyWidth * blackKeyMult;
+
+    for (int octave = 0; octave < octaves; ++octave) {
+
+        const int octaveStart = 12*octave;
+
+        p.setBrush(Qt::white);
+        for (int key = 0; key < 7; ++key) {
+            bool keyIsPlaying = (m_currentMouseNote == rootnote + octaveStart + whiteIndex[key]);
+
+            if (keyIsPlaying) p.setBrush(Qt::red);
+            p.drawRect((7*octave + key) * whiteKeyWidth, 0, whiteKeyWidth, height());
+            if (keyIsPlaying) p.setBrush(Qt::white);
+        }
+
+        //now draw black keys
+        p.setBrush(Qt::black);
+        for (int key = 0; key < 7; ++key) {
+
+            if (key == 2 || key == 6) continue;
+
+            const int blackOffset = 0.5 * whiteKeyWidth + blackKeyWidth / 4;
+            bool keyIsPlaying = (m_currentMouseNote == rootnote + octaveStart + blackIndex[key]);
+
+            if (keyIsPlaying) p.setBrush(Qt::red);
+            p.drawRect((7*octave + key) * whiteKeyWidth + blackOffset, 0, blackKeyWidth, height() * blackKeyHeight);
+            if (keyIsPlaying) p.setBrush(Qt::black);
+        }
+    }
 }
 
 void Keyboard::mousePressEvent(QMouseEvent *event)
@@ -68,22 +143,6 @@ void Keyboard::mouseMoveEvent(QMouseEvent *event)
     update();
 }
 
-void Keyboard::paintEvent(QPaintEvent *event)
-{
-    QPainter p(this);
-
-    int tangentWidth = width() / tangents;
-
-    for (int i = 0; i < tangents; ++i) {
-        if (i != (m_currentMouseNote - rootnote)) {
-            p.drawRect(i*tangentWidth, 0, tangentWidth, height() / 2);
-        } else {
-            p.setBrush(Qt::black);
-            p.fillRect((m_currentMouseNote - rootnote)*tangentWidth, 0, tangentWidth, height() / 2, Qt::SolidPattern);
-            p.setBrush(QBrush());
-        }
-    }
-}
 
 void Keyboard::mouseReleaseEvent(QMouseEvent *event)
 {
